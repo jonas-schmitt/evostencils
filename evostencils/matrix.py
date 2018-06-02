aac6b6888a886ae602d2717931b4cc7d3117dc45
Jonas Schmitt <jonas.schmitt@fau.de>
2018-06-02 22:03:09 +0200

    Matrix types and symbols are now treated separately. The newly created symbol classes (derived from sympy.MatrixSymbol) for diagonal, lower and upper triangular matrices store their source matrix to enable code generation for these sympy nodes.

--- evostencils/matrix.py
+++ evostencils/matrix.py
@@ -1,88 +1,81 @@
-#import sympy as sp
 from sympy import MatrixSymbol
 from functools import reduce
 import operator
 
+#class SparseMatrixSymbol(MatrixSymbol):
+#    def __new__(cls, name, m, n):
+#        obj = super().__new__(cls, name, m, n)
+#        obj._matrix_type = None
+#        return obj
+#
+#    def set_matrix_type(self, matrix_type):
+#        self._matrix_type = matrix_type
+#
+#    @property
+#    def get_matrix_type(self):
+#        return self._matrix_type
 
-class VectorType(MatrixSymbol):
-    def __new__(cls, name, m, n):
-        obj = super().__new__(cls, name, m, 1)
-        return obj
 
-
-class MatrixType(MatrixSymbol):
+class SplittedMatrixSymbol(MatrixSymbol):
     def __new__(cls, name, m, n):
         obj = super().__new__(cls, name, m, n)
+        obj._source_matrix = None
         return obj
 
-class SplittedMatrix(MatrixSymbol):
-    def __new__(cls, name, source_matrix):
-        obj = super().__new__(cls, name, source_matrix.shape[0], source_matrix.shape[1])
-        obj._source_matrix = source_matrix
-        return obj
+    def set_source_matrix(self, source_matrix):
+        self._source_matrix = source_matrix
 
     @property
     def get_source_matrix(self):
         return self._source_matrix
 
 
-class DiagonalMatrixType(SplittedMatrix):
-    def __new__(cls, source_matrix):
-        return super().__new__(cls, f"{source_matrix.name}.diag", source_matrix)
+def get_diagonal(A) -> SplittedMatrixSymbol:
+    D = SplittedMatrixSymbol(f"{A.name}.diagonal", *A.shape)
+    D.set_source_matrix(A)
+    return D
 
 
-class TriangularMatrixType(SplittedMatrix):
-    def __new__(cls, name, source_matrix):
-        obj = super().__new__(cls, name, source_matrix)
-        return obj
+def get_lower_triangle(A) -> SplittedMatrixSymbol:
+    L = SplittedMatrixSymbol(f"{A.name}.lower_triangle", *A.shape)
+    L.set_source_matrix(A)
+    return L
 
 
-class LowerTriangularMatrixType(TriangularMatrixType):
-    def __new__(cls, source_matrix):
-        return super().__new__(cls, f"{source_matrix.name}.lower", source_matrix)
+def get_upper_triangle(A) -> SplittedMatrixSymbol:
+    U = SplittedMatrixSymbol(f"{A.name}.upper_triangle", *A.shape)
+    U.set_source_matrix(A)
+    return U
 
 
-class UpperTriangularMatrixType(TriangularMatrixType):
-    def __new__(cls, source_matrix):
-        return super().__new__(cls, f"{source_matrix.name}.upper", source_matrix)
-
-
-def generate_vector_on_grid(name: str, grid_size: tuple) -> VectorType:
+def generate_vector_on_grid(name: str, grid_size: tuple) -> MatrixSymbol:
     n = reduce(operator.mul, grid_size, 1)
-    return VectorType(name, n, 1)
+    return MatrixSymbol(name, n, 1)
 
 
-def generate_matrix_on_grid(name: str, grid_size: tuple) -> MatrixType:
+def generate_matrix_on_grid(name: str, grid_size: tuple) -> MatrixSymbol:
     n = reduce(operator.mul, grid_size, 1)
-    return MatrixType(name, n, n)
+    return MatrixSymbol(name, n, n)
 
 
+class MatrixTypeMetaClass(type):
+    def __new__(mcs, class_name, bases, dct):
+        return super(MatrixTypeMetaClass, mcs).__new__(mcs, class_name, bases, dct)
 
-# def diagonal(matrix_symbol: MatrixSymbol) -> MatrixSymbol:
-#     return MatrixSymbol(str(matrix_symbol) + ".diag", *matrix_symbol.shape)
-#
-#
-# def lower(matrix_symbol: MatrixSymbol) -> MatrixSymbol:
-#     return MatrixSymbol(str(matrix_symbol) + ".lower", *matrix_symbol.shape)
-#
-#
-# def upper(matrix_symbol: MatrixSymbol) -> MatrixSymbol:
-#     return MatrixSymbol(str(matrix_symbol) + ".upper", *matrix_symbol.shape)
-#
-#
-# class MatrixMetaClass(type):
-#     def __new__(mcs, class_name, bases, dct):
-#         return super(MatrixMetaClass, mcs).__new__(mcs, class_name, bases, dct)
-#
-#     def __eq__(self, other):
-#         return self.invertible == other.invertible and self.shape == other.shape
-#
-#     def __hash__(self):
-#         return hash((*self.shape, self.invertible))
-#
-#
-# def generate_matrix_type(shape, invertible):
-#     return MatrixMetaClass("MatrixType", (), {
-#         "shape": shape,
-#         "invertible": invertible
-#     })
+    def __eq__(self, other):
+        return self.shape == other.shape \
+               and self.diagonal == other.diagonal \
+               and self.lower_triangle == other.lower_triangle \
+               and self.upper_triangle == other.upper_triangle
+
+    def __hash__(self):
+        return hash((*self.shape, self.diagonal, self.lower_triangle, self.upper_triangle))
+
+
+def generate_matrix_type(shape_, diag=True, lower_triangle=True, upper_triangle=True):
+    return MatrixTypeMetaClass("MatrixType", (), {
+        "shape": shape_,
+        "diagonal": diag,
+        "lower_triangle": lower_triangle,
+        "upper_triangle": upper_triangle
+    })
--- playground/generate_expression.py
+++ playground/generate_expression.py
@@ -8,9 +8,17 @@ x = generate_vector_on_grid('x', grid)
 b = generate_vector_on_grid('b', grid)
 A = generate_matrix_on_grid('A', grid)
 I = Identity(x.shape[0])
-D = DiagonalMatrix(A)
-L = LowerTriangularMatrixType(A)
-U = UpperTriangularMatrixType(A)
+D = get_diagonal(A)
+L = get_lower_triangle(A)
+U = get_upper_triangle(A)
+
+MatrixType = generate_matrix_type(A.shape)
+VectorType = generate_matrix_type(x.shape)
+DiagonalMatrixType = generate_matrix_type(A.shape, diag=True, lower_triangle=False, upper_triangle=False)
+StrictlyLowerTriangularMatrixType = generate_matrix_type(A.shape, diag=False, lower_triangle=True, upper_triangle=False)
+StrictlyUpperTriangularMatrixType = generate_matrix_type(A.shape, diag=False, lower_triangle=False, upper_triangle=True)
+LowerTriangularMatrixType = generate_matrix_type(A.shape, diag=True, lower_triangle=True, upper_triangle=False)
+UpperTriangularMatrixType = generate_matrix_type(A.shape, diag=True, lower_triangle=False, upper_triangle=True)
 
 pset = gp.PrimitiveSetTyped("main", [], VectorType)
 
@@ -19,8 +27,8 @@ pset.addTerminal(x, VectorType, name='x')
 pset.addTerminal(b, VectorType, name='b')
 pset.addTerminal(A, MatrixType, name='A')
 pset.addTerminal(D, DiagonalMatrixType, name='D')
-pset.addTerminal(L, LowerTriangularMatrixType, name='L')
-pset.addTerminal(U, UpperTriangularMatrixType, name='U')
+pset.addTerminal(L, StrictlyLowerTriangularMatrixType, name='L')
+pset.addTerminal(U, StrictlyUpperTriangularMatrixType, name='U')
 pset.addTerminal(D.I, DiagonalMatrixType, name='D.I')
 pset.addTerminal(I, DiagonalMatrixType, name='I')
 
@@ -32,14 +40,7 @@ pset.addPrimitive(MatrixExpr.__mul__, [DiagonalMatrixType, VectorType], VectorTy
 pset.addPrimitive(MatrixExpr.__mul__, [DiagonalMatrixType, DiagonalMatrixType], DiagonalMatrixType, name='mul')
 pset.addPrimitive(MatrixExpr.__mul__, [DiagonalMatrixType, MatrixType], MatrixType, name='mul')
 pset.addPrimitive(MatrixExpr.__mul__, [MatrixType, DiagonalMatrixType], MatrixType, name='mul')
-#
-pset.addPrimitive(MatrixExpr.__add__, [MatrixType, MatrixType], MatrixType, name='add')
-pset.addPrimitive(MatrixExpr.__add__, [DiagonalMatrixType, DiagonalMatrixType], DiagonalMatrixType, name='add')
-pset.addPrimitive(MatrixExpr.__add__, [LowerTriangularMatrixType, LowerTriangularMatrixType], LowerTriangularMatrixType, name='add')
-pset.addPrimitive(MatrixExpr.__add__, [UpperTriangularMatrixType, UpperTriangularMatrixType], UpperTriangularMatrixType, name='add')
-pset.addPrimitive(MatrixExpr.__add__, [VectorType, VectorType], VectorType, name='add')
-pset.addPrimitive(MatrixExpr.inverse, [DiagonalMatrixType], DiagonalMatrixType, "inverse")
-
+# TODO include operations on remaining matrix types
 
 creator.create("Individual", gp.PrimitiveTree)
 
