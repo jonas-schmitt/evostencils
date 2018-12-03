9d263cb112f5f72aebe01f162a262f907736d6aa
Jonas Schmitt <jonas.schmitt@fau.de>
2018-12-03 16:05:31 +0100

    Adapted ExaStencils test cases

--- evostencils/exastencils/gallery/finite_differences/poisson_2D.py
+++ evostencils/exastencils/gallery/finite_differences/poisson_2D.py
@@ -1,23 +1,23 @@
 
-def global_declarations():
-    return ""
+class Declarations:
+    @staticmethod
+    def get_globals():
+        return ''
 
-
-def operator():
-    return """
-Operator Laplace from Stencil {
+    @staticmethod
+    def get_operator_stencil():
+        return """
     [ 0,  0] =>  2.0 / ( vf_gridWidth_x ** 2 ) + 2.0 / ( vf_gridWidth_y ** 2 )
     [-1,  0] => -1.0 / ( vf_gridWidth_x ** 2 )
     [ 1,  0] => -1.0 / ( vf_gridWidth_x ** 2 )
     [ 0, -1] => -1.0 / ( vf_gridWidth_y ** 2 )
     [ 0,  1] => -1.0 / ( vf_gridWidth_y ** 2 )
-}
-"""
+    """
 
+    @staticmethod
+    def get_boundary_as_string():
+        return 'cos ( PI * vf_boundaryPos_x ) - sin ( 2.0 * PI * vf_boundaryPos_y )\n'
 
-def boundary():
-    return 'cos ( PI * vf_boundaryPos_x ) - sin ( 2.0 * PI * vf_boundaryPos_y )\n'
-
-
-def rhs():
-    return 'PI**2 * cos ( PI * vf_nodePos_x ) - 4.0 * PI**2 * sin ( 2.0 * PI * vf_nodePos_y )\n'
+    @staticmethod
+    def get_rhs_as_string():
+        return 'PI**2 * cos ( PI * vf_nodePos_x ) - 4.0 * PI**2 * sin ( 2.0 * PI * vf_nodePos_y )\n'
--- evostencils/exastencils/gallery/finite_differences/var_coeff_2D.py
+++ evostencils/exastencils/gallery/finite_differences/var_coeff_2D.py
@@ -1,28 +1,30 @@
-def global_declarations():
-    return """
+class Declarations:
+    @staticmethod
+    def get_globals():
+            return """
 Globals {
     Val kappa : Real = 10.0
 }
-"""
 
-
-def operator():
-    return """
 Function getCoefficient ( xPos : Real, yPos : Real ) : Real {
     return exp ( ( kappa * ( ( xPos - ( xPos ** 2 ) ) * ( yPos - ( yPos ** 2 ) ) ) ) )
 }
+"""
 
-Stencil LaplaceStencil@all{
+    @staticmethod
+    def get_operator_stencil():
+        return """
     [0, 0] => ( ( ( getCoefficient ( ( vf_nodePosition_x@current + ( 0.5 * vf_gridWidth_x@current ) ), vf_nodePosition_y@current ) + getCoefficient ( ( vf_nodePosition_x@current - ( 0.5 * vf_gridWidth_x@current ) ), vf_nodePosition_y@current ) ) / ( vf_gridWidth_x@current * vf_gridWidth_x@current ) ) + ( ( getCoefficient ( vf_nodePosition_x@current, ( vf_nodePosition_y@current + ( 0.5 * vf_gridWidth_y@current ) ) ) + getCoefficient ( vf_nodePosition_x@current, ( vf_nodePosition_y@current - ( 0.5 * vf_gridWidth_y@current ) ) ) ) / ( vf_gridWidth_y@current * vf_gridWidth_y@current ) ) )
     [1, 0] => ( ( -1.0 * getCoefficient ( ( vf_nodePosition_x@current + ( 0.5 * vf_gridWidth_x@current ) ), vf_nodePosition_y@current ) ) / ( vf_gridWidth_x@current * vf_gridWidth_x@current ) )
     [-1, 0] => ( ( -1.0 * getCoefficient ( ( vf_nodePosition_x@current - ( 0.5 * vf_gridWidth_x@current ) ), vf_nodePosition_y@current ) ) / ( vf_gridWidth_x@current * vf_gridWidth_x@current ) )
     [0, 1] => ( ( -1.0 * getCoefficient ( vf_nodePosition_x@current, ( vf_nodePosition_y@current + ( 0.5 * vf_gridWidth_y@current ) ) ) ) / ( vf_gridWidth_y@current * vf_gridWidth_y@current ) )
     [0, -1] => ( ( -1.0 * getCoefficient ( vf_nodePosition_x@current, ( vf_nodePosition_y@current - ( 0.5 * vf_gridWidth_y@current ) ) ) ) / ( vf_gridWidth_y@current * vf_gridWidth_y@current ) )
-}
-"""
+    """
 
-def boundary():
-    return '( 1.0 - exp ( ( ( -1.0 * kappa ) * ( ( vf_boundaryCoord_x@current - ( vf_boundaryCoord_x@current ** 2 ) ) * ( vf_boundaryCoord_y@current - ( vf_boundaryCoord_y@current ** 2 ) ) ) ) ) )\n'
+    @staticmethod
+    def get_boundary_as_string():
+        return '( 1.0 - exp ( ( ( -1.0 * kappa ) * ( ( vf_boundaryCoord_x@current - ( vf_boundaryCoord_x@current ** 2 ) ) * ( vf_boundaryCoord_y@current - ( vf_boundaryCoord_y@current ** 2 ) ) ) ) ) )\n'
 
-def rhs():
-    return '( ( 2.0 * kappa ) * ( ( vf_nodePosition_x@current - ( vf_nodePosition_x@current ** 2 ) ) + ( vf_nodePosition_y@current - ( vf_nodePosition_y@current ** 2 ) ) ) )\n'
+    @staticmethod
+    def get_rhs_as_string():
+        return '( ( 2.0 * kappa ) * ( ( vf_nodePosition_x@current - ( vf_nodePosition_x@current ** 2 ) ) + ( vf_nodePosition_y@current - ( vf_nodePosition_y@current ** 2 ) ) ) )\n'
