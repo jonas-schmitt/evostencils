bb0d76f3fc69754ce8eb4e2fe7362c456062a81a
Jonas Schmitt <jonas.schmitt@fau.de>
2019-07-01 16:50:31 +0200

    Implemented multigrid initialization for systems. Adapted prolongation and restriction.

--- evostencils/expressions/system.py
+++ evostencils/expressions/system.py
@@ -70,31 +70,20 @@ class ZeroApproximation(Approximation):
         super().__init__(name, [base.ZeroApproximation(g) for g in grid])
 
 
-class InterGridOperator(Operator):
-    def __init__(self, name, scalar_intergrid_operator, scalar_zero_operator, grid: [base.Grid]):
-        number_of_unknowns = len(grid)
-        entries = [[] for _ in range(number_of_unknowns)]
-        for i in range(number_of_unknowns):
-            for j in range(number_of_unknowns):
-                if i == j:
-                    entries[i].append(scalar_intergrid_operator)
-                else:
-                    entries[j].append(scalar_zero_operator)
+class Restriction(Operator):
+    def __init__(self, name, fine_grid: [base.Grid], coarse_grid: [base.Grid], stencil_generator=None):
+        entries = [[multigrid.Restriction(name, fg, cg, stencil_generator)
+                    if i == j else multigrid.ZeroRestriction(fg, cg) for j in range(len(fine_grid))]
+                   for i, (fg, cg) in enumerate(zip(fine_grid, coarse_grid))]
         super().__init__(name, entries)
 
 
-class Restriction(InterGridOperator):
-    def __init__(self, name, scalar_restriction_operator, grid: [base.Grid]):
-        zero_operator = multigrid.ZeroRestriction(scalar_restriction_operator.fine_grid,
-                                                  scalar_restriction_operator.coarse_grid)
-        super().__init__(name, scalar_restriction_operator, zero_operator, grid)
-
-
-class Prolongation(InterGridOperator):
-    def __init__(self, name, scalar_prolongation_operator, grid: [base.Grid]):
-        zero_operator = multigrid.ZeroProlongation(scalar_prolongation_operator.fine_grid,
-                                                  scalar_prolongation_operator.coarse_grid)
-        super().__init__(name, scalar_prolongation_operator, zero_operator, grid)
+class Prolongation(Operator):
+    def __init__(self, name, fine_grid: [base.Grid], coarse_grid: [base.Grid], stencil_generator=None):
+        entries = [[multigrid.Prolongation(name, fg, cg, stencil_generator)
+                    if i == j else multigrid.ZeroProlongation(fg, cg) for j in range(len(fine_grid))]
+                   for i, (fg, cg) in enumerate(zip(fine_grid, coarse_grid))]
+        super().__init__(name, entries)
 
 
 class Diagonal(base.UnaryExpression):
@@ -108,3 +97,8 @@ class ElementwiseDiagonal(base.UnaryExpression):
 def get_coarse_grid(grid: [base.Grid], coarsening_factor):
     return list(map(lambda g: multigrid.get_coarse_grid(g, coarsening_factor), grid))
 
+
+def get_coarse_operator(operator, coarse_grid):
+    new_entries = [[base.Operator(f'{entry.name}_c', coarse_grid[i], entry.stencil_generator) for entry in row]
+                   for i, row in enumerate(operator.entries)]
+    return Operator(f'{operator.name}_c', new_entries)
