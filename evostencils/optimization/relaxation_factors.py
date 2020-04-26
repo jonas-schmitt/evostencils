f24a66904384323a38772295a3bd4155871d6c99
Jonas Schmitt <jonas.schmitt@fau.de>
2020-04-26 14:28:44 +0200

    Skip krylov subspace method nodes in the relaxation factor optimization

--- evostencils/optimization/relaxation_factors.py
+++ evostencils/optimization/relaxation_factors.py
@@ -1,5 +1,5 @@
 from deap import creator, tools, algorithms, cma
-from evostencils.expressions import base, system
+from evostencils.expressions import base, system, krylov_subspace
 import deap
 import numpy
 from math import log
@@ -23,7 +23,9 @@ def reset_status(expression: base.Expression):
 
 def set_relaxation_factors(expression: base.Expression, weights: list) -> list:
     if isinstance(expression, base.Cycle):
-        if not expression.weight_set:
+        if not expression.weight_set and \
+                not (isinstance(expression.correction, base.Multiplication) and
+                     isinstance(expression.correction.operand1, krylov_subspace.KrylovSubspaceMethod)):
             head, *tail = weights
             expression._relaxation_factor = head
             expression.weight_set = True
@@ -46,7 +48,9 @@ def set_relaxation_factors(expression: base.Expression, weights: list) -> list:
 def obtain_relaxation_factors(expression: base.Expression) -> list:
     weights = []
     if isinstance(expression, base.Cycle):
-        if not expression.weight_obtained:
+        if not expression.weight_obtained and \
+                not (isinstance(expression.correction, base.Multiplication) and
+                     isinstance(expression.correction.operand1, krylov_subspace.KrylovSubspaceMethod)):
             weights.append(expression.relaxation_factor)
             expression.weight_obtained = True
         weights.extend(obtain_relaxation_factors(expression.correction))
