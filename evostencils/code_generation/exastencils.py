9a7be2022c7cf42c73dc0dbc873f911e1b44da15
Jonas Schmitt <jonas.schmitt@fau.de>
2020-08-16 09:35:29 +0200

    Fixed computation of the average number of iterations in evaluation

--- evostencils/code_generation/exastencils.py
+++ evostencils/code_generation/exastencils.py
@@ -371,7 +371,7 @@ class ProgramGenerator:
             total_time += time_to_solution
             sum_of_convergence_factors += convergence_factor
             total_number_of_iterations += number_of_iterations
-        return total_time / number_of_samples, sum_of_convergence_factors / number_of_samples, number_of_iterations / number_of_samples
+        return total_time / number_of_samples, sum_of_convergence_factors / number_of_samples, total_number_of_iterations / number_of_samples
 
     def initialize_code_generation(self, min_level: int, max_level: int, iteration_limit=10000):
         knowledge_path = self.generate_level_adapted_knowledge_file(min_level, max_level)
